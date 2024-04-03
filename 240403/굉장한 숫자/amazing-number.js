3c0d9be2bfc066a625c2c2037618a579722f03eb
Jooyoung Yang <89117851+yangddu@users.noreply.github.com>
2024-04-03 09:38:02 +0900

    Title: 굉장한 숫자, Time: 229ms, Memory: 27MB, Status: Passed - CodeTree

--- 240403/굉장한 숫자/amazing-number.js
+++ 240403/굉장한 숫자/amazing-number.js
@@ -2,5 +2,8 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim();
 let n = Number(input);
 
-if ((n % 2 === 1 && n % 3 === 0) || (n % 2 === 0 || n % 5 === 0)) console.log('true')
+let a = (n % 2 === 1) && (n % 3 === 0);
+let b = (n % 2 === 0) && (n % 5 === 0);
+
+if (a || b) console.log('true');
 else console.log('false');
