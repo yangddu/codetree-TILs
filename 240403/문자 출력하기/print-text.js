5cce31a99061f9cbf317f602d203fd5dcd53f5e1
Jooyoung Yang <89117851+yangddu@users.noreply.github.com>
2024-04-03 19:53:55 +0900

    Title: 문자 출력하기, Time: 188ms, Memory: 14MB, Status: Passed - CodeTree

--- 240403/문자 출력하기/print-text.js
+++ 240403/문자 출력하기/print-text.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-let input = fs.readFileSync(0).toString();
+let input = fs.readFileSync(0).toString().trim();
 let result = '';
 for (let i = 0; i < 8; i++) {
     result += input + '';
