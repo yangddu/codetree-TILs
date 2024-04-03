197bf3bf6c7f4fb3387366bfe2fa23d0d7b5141e
Jooyoung Yang <89117851+yangddu@users.noreply.github.com>
2024-04-03 09:49:07 +0900

    Title: 장학금, Time: 341ms, Memory: 14MB, Status: Passed - CodeTree

--- 240403/장학금/scholarship.js
+++ 240403/장학금/scholarship.js
@@ -5,9 +5,9 @@ let a = Number(input[0]);
 let b = Number(input[1]);
 
 if (a >= 90 && b >= 95) {
-    console.log(10);
+    console.log(100000);
 } else if (a >= 90 && b >= 90) {
-    console.log(5);
+    console.log(50000);
 } else {
     console.log(0);
 }
