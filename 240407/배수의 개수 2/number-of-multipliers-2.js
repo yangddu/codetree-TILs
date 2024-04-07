66f73abd6ec75b0d78781d0e3dbb0d39fc8cdda2
Jooyoung Yang <89117851+yangddu@users.noreply.github.com>
2024-04-07 17:56:19 +0900

    Title: 배수의 개수 2, Time: 264ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240407/배수의 개수 2/number-of-multipliers-2.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+let cnt = 0; 
+for (let i = 0; i < input.length; i++) {
+    if (input[i] % 2 === 1) {
+        cnt ++;
+    }
+}
+console.log(cnt);
