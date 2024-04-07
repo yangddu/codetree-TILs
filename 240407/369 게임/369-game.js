f8b8ca8d69a3ac0c3d6345352e344f2884916a8e
Jooyoung Yang <89117851+yangddu@users.noreply.github.com>
2024-04-07 17:04:55 +0900

    Title: 369 게임, Time: 179ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240407/369 게임/369-game.js
+++ 240407/369 게임/369-game.js
@@ -4,7 +4,7 @@ let n = Number(input);
 let result = '';
 
 for (let i = 1; i <= n; i++) {
-    if ((i % 10 === 3) || (i % 10 === 6) || (i % 10 === 9) || (i % 3 === 0)) {
+    if ((i % 10 === 3) || (i % 10 === 6) || (i % 10 === 9) || (i % 3 === 0) || (parseInt((parseInt(i / 3)) / 10) === 1) || (parseInt((parseInt(i / 6)) / 10) === 1) || (parseInt((parseInt(i / 9)) / 10) === 1)) {
         result += '0 ';
     } else {
         result += i + ' ';
