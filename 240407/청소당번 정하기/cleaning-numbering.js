33636cfddb5861a9081d8ddfba0ff28a6f57640b
Jooyoung Yang <89117851+yangddu@users.noreply.github.com>
2024-04-07 18:37:26 +0900

    Title: 청소당번 정하기, Time: 216ms, Memory: 15MB, Status: Passed - CodeTree

--- 240407/청소당번 정하기/cleaning-numbering.js
+++ 240407/청소당번 정하기/cleaning-numbering.js
@@ -1,17 +1,15 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim();
 let n = Number(input);
-let cnt1 = 0;
-let cnt2 = 0;
-let cnt3 = 0;
+let cntA = 0, cntB = 0, cntC = 0;
 
 for (let i = 1; i <= n; i ++) {
-    if (i % 2 === 0 && i % 6 !== 0 && i % 12 !== 0) {
-        cnt1++;
-    } else if ((i % 3 === 0 || i % 6 === 0) && i % 12 !== 0) {
-        cnt2++;
-    } else if (i % 12 === 0) {
-        cnt3++;
+    if (i % 12 === 0) {
+        cntC++;
+    } else if (i % 3 === 0) {
+        cntB++;
+    } else if (i % 2 === 0) {
+        cntA++;
     }
 }
-console.log(cnt1, cnt2, cnt3);
+console.log(cntA, cntB, cntC);
