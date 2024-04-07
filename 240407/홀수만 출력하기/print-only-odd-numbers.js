df534880a5c7342bed45792eff59517bd4867843
Jooyoung Yang <89117851+yangddu@users.noreply.github.com>
2024-04-07 17:36:36 +0900

    Title: 홀수만 출력하기, Time: 163ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240407/홀수만 출력하기/print-only-odd-numbers.js
+++ 240407/홀수만 출력하기/print-only-odd-numbers.js
@@ -3,6 +3,6 @@ let input = fs.readFileSync(0).toString().trim().split("\n");
 
 for (let i = 0; i < input.length; i ++) {
     if ((Number(input[i]) % 2 === 1) && (Number(input[i]) % 3 === 0)) {
-        console.log(input[i]);
+        console.log(Number(input[i]));
     }
 }
