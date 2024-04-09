31d193002125ec0828dd1cc056aced4e0376ddba
Jooyoung Yang <89117851+yangddu@users.noreply.github.com>
2024-04-09 17:44:01 +0900

    Title: 총합 구하기, Time: 219ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240409/총합 구하기/find-the-total.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0]);
+let b = Number(input[1]);
+let sum = 0;
+
+for (let i = a; i <= b; i++) {
+    if (i % 6 === 0 && i % 8 !== 0) {
+        sum += i;
+    }
+}
+console.log(sum);
