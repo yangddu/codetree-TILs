904faed138ee29bed1bdf3b48ceccc9343e14ad1
Jooyoung Yang <89117851+yangddu@users.noreply.github.com>
2024-04-09 17:05:26 +0900

    Title: 5의 배수의 합, Time: 224ms, Memory: 22MB, Status: Passed - CodeTree

---
+++ 240409/5의 배수의 합/sum-of-specific-condition-multiple.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0]);
+let b = Number(input[1]);
+let sum = 0;
+
+if (a > b) {
+    for (let i = b; i <= a; i ++) {
+        if (i % 5 === 0) {
+            sum += i;
+        }
+    }
+} else if (b > a) {
+    for (let i = a; i <= b; i ++) {
+        if (i % 5 === 0) {
+            sum += i;
+        }
+    }
+}
+console.log(sum);
