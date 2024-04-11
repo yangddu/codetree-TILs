61c47d7dd9f3af425487d795d16ca042ef1fee8d
Jooyoung Yang <89117851+yangddu@users.noreply.github.com>
2024-04-11 11:24:32 +0900

    Title: 1부터의 합, Time: 165ms, Memory: 14MB, Status: Passed - CodeTree

--- 240411/1부터의 합/sum-from-the-one.js
+++ 240411/1부터의 합/sum-from-the-one.js
@@ -3,13 +3,10 @@ let input = fs.readFileSync(0).toString().trim();
 
 let n = Number(input);
 let sum = 0;
-let prod = 0;
-
 for (let i = 1; i <= 100; i++) {
     sum += i;
     if (sum >= n) {
+        console.log(i);
         break;
     }
-    prod += i;
 }
-console.log(prod);
