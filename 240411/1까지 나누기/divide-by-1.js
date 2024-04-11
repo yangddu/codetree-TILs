31df1d4a398936b7ad7dc0cfd8a8b2443c604f38
Jooyoung Yang <89117851+yangddu@users.noreply.github.com>
2024-04-11 15:20:36 +0900

    Title: 1까지 나누기, Time: 183ms, Memory: 22MB, Status: Wrong Answer - CodeTree

--- 240411/1까지 나누기/divide-by-1.js
+++ 240411/1까지 나누기/divide-by-1.js
@@ -4,11 +4,11 @@ let n = Number(input);
 let cnt = 0;
 
 for (let i = 1; i <= n; i++) {
-    cnt++;
     n = parseInt(n / i);
-    if ((n / i + 1) <= 1) {
+    if (n < i + 1) {
       cnt ++;
       break;
     }
+    cnt ++;
 }
 console.log(cnt);
