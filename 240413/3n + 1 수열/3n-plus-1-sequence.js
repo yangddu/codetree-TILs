b4b8d3d68529f789021072519afdc4901716406d
Jooyoung Yang <89117851+yangddu@users.noreply.github.com>
2024-04-13 18:14:41 +0900

    Title: 3n + 1 수열, Time: 182ms, Memory: 16MB, Status: Passed - CodeTree

--- 240413/3n + 1 수열/3n-plus-1-sequence.js
+++ 240413/3n + 1 수열/3n-plus-1-sequence.js
@@ -4,14 +4,14 @@ let n = Number(input);
 let cnt = 0;
 
 while (true) {
+    if (n === 1) {
+        console.log(cnt);
+        break;
+    }
     if (n % 2 === 0) {
         n /= 2;
     } else {
         n = (3 * n) + 1;
     }
     cnt ++;
-    if (n === 1) {
-        console.log(cnt);
-        break;
-    }
 }
