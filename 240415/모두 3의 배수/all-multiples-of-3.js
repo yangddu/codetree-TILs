df3f355c420766775ef5d8314f6e7ece3e5921c4
Jooyoung Yang <89117851+yangddu@users.noreply.github.com>
2024-04-15 22:59:21 +0900

    Title: 모두 3의 배수, Time: 298ms, Memory: 14MB, Status: Passed - CodeTree

--- 240415/모두 3의 배수/all-multiples-of-3.js
+++ 240415/모두 3의 배수/all-multiples-of-3.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
-let result = false;
+let result = true;
 
 for (let i = 0; i < input.length; i++) {
-    if (input[i] % 3 === 0) {
-        result = true; 
+    if (input[i] % 3 !== 0) {
+        result = false;
     }
 }
 
