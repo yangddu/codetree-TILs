75de6c511ce05638b904e47876117b84b3f6eff1
Jooyoung Yang <89117851+yangddu@users.noreply.github.com>
2024-04-17 15:04:51 +0900

    Title: 정사각형 두 개 출력, Time: 181ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240417/정사각형 두 개 출력/output-two-rectangles.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim();
+let n = Number(input);
+let str = '';
+
+for (let i = 0; i < n; i++) {
+    str = '';
+    for (let j = 0; j < n; j++) {
+        str += '*';
+    }
+    console.log(str);
+}
+console.log('');
+for (let i = 0; i < n; i++) {
+    str = '';
+    for (let j = 0; j < n; j++) {
+        str += '*';
+    }
+    console.log(str);
+}
