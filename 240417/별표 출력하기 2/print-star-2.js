fbb615e0517159aa21077ebd6e76d8cb38a2c894
Jooyoung Yang <89117851+yangddu@users.noreply.github.com>
2024-04-17 16:35:42 +0900

    Title: 별표 출력하기 2, Time: 173ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240417/별표 출력하기 2/print-star-2.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim();
+let n = Number(input);
+let str = '';
+
+for (let i = 1; i <= n; i++) {
+    str = '';
+    for (let j = n; j >= i; j--) {
+        str += '* ';
+    }
+    console.log(str);
+}
